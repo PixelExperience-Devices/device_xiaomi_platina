e12f146cafe86226b0ce6078b7622ec9cc1c1c50
Wei Wang <wvw@google.com>
2017-12-31 22:14:16 -0800

    platina: power: switch to libperfmgr for powerhint
    Test: boot and do powerhint
    Bug: 62041945
    Change-Id: I7de1d2bf377fb46162171a084fca3413b1067d3b
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>
    Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>

--- device.mk
+++ device.mk
@@ -330,7 +330,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service.jason-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    device/xiaomi/platina/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
