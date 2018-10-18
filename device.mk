dc0efb59efcb256611056321940b76ab69b64f4b
Demon Singur <demonsingur@gmail.com>
2018-10-18 10:38:04 +0000

    platina: Remove qti perf stack
    Change-Id: I4ddac4cd7a9300a0f57c11c805e14d398b446213
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- device.mk
+++ device.mk
@@ -348,8 +348,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@2.0.vendor
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
