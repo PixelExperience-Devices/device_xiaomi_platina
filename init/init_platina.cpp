679c5dad8337f4e40270085f1c9900bbf21f943f
lihonglinglory <lihonglin10086@163.com>
2018-11-25 17:00:18 +0800

    platina: libinit: Set product model

--- init/init_platina.cpp
+++ init/init_platina.cpp
@@ -25,14 +25,10 @@
    OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-#include <stdio.h>
-#include <stdlib.h>
+#include <fstream>
 #include <unistd.h>
 
-#include <android-base/file.h>
 #include <android-base/properties.h>
-#include <android-base/strings.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
@@ -40,77 +36,49 @@
 #include "vendor_init.h"
 
 using android::base::GetProperty;
-using android::base::ReadFileToString;
-using android::base::Trim;
 using android::init::property_set;
 
 void property_override(char const prop[], char const value[])
 {
     prop_info *pi;
-
-    pi = (prop_info*) __system_property_find(prop);
+     pi = (prop_info*) __system_property_find(prop);
     if (pi)
         __system_property_update(pi, value, strlen(value));
     else
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
-
-void property_override_dual(char const system_prop[],
+ void property_override_dual(char const system_prop[],
         char const vendor_prop[], char const value[])
 {
     property_override(system_prop, value);
     property_override(vendor_prop, value);
 }
 
-static void init_alarm_boot_properties()
+static void init_setup_model_properties()
 {
-    char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
-    char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
-    std::string boot_reason;
-    std::string power_off_alarm;
-    std::string reboot_reason = GetProperty("ro.boot.alarmboot", "");
+    std::ifstream fin;
+    std::string buf;
 
-    if (ReadFileToString(boot_reason_file, &boot_reason)
-            && ReadFileToString(power_off_alarm_file, &power_off_alarm)) {
-        /*
-         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
-         * For existing PMIC chips, the following mapping applies
-         * for the value of boot_reason:
-         *
-         * 0 -> unknown
-         * 1 -> hard reset
-         * 2 -> sudden momentary power loss (SMPL)
-         * 3 -> real time clock (RTC)
-         * 4 -> DC charger inserted
-         * 5 -> USB charger inserted
-         * 6 -> PON1 pin toggled (for secondary PMICs)
-         * 7 -> CBLPWR_N pin toggled (for external power supply)
-         * 8 -> KPDPWR_N pin toggled (power key pressed)
-         */
-         if ((Trim(boot_reason) == "3" || reboot_reason == "true")
-                 && Trim(power_off_alarm) == "1") {
-             property_set("ro.alarm_boot", "true");
-         } else {
-             property_set("ro.alarm_boot", "false");
-         }
+    fin.open("/proc/cmdline");
+    while (std::getline(fin, buf, ' '))
+        if (buf.find("androidboot.hwc") != std::string::npos)
+            break;
+    fin.close();
+
+    if (buf.find("CN") != std::string::npos) {
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "MI 8 Lite");
+    } else {
+        property_override_dual("ro.product.model", "ro.vendor.product.model",  "?MI 8 Lite");
     }
 }
 
 void vendor_load_properties()
 {
     std::string platform;
-    std::string hw_device;
 
     platform = GetProperty("ro.board.platform", "");
     if (platform != ANDROID_TARGET)
         return;
 
-    hw_device = GetProperty("ro.board.variant", "");
-    if (hw_device == "d9p") {
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "MI PAD 4 PLUS");
-    } else if (hw_device == "d9") {
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "MI PAD 4");
-    }
-
-    init_alarm_boot_properties();
+    init_setup_model_properties();
 }
