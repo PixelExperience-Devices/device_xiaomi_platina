8c713098f941575beba2ca7a11a2ac34128f4f39
lihonglinglory <lihonglin10086@163.com>
2018-12-04 20:57:32 +0800

    platina: Disabled DoubleTapWake for a moment

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,7 +156,7 @@ SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
 VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 
 # DT2W
-TARGET_TAP_TO_WAKE_NODE := "/sys/class/input/input2/wake_gesture"
+#TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc/c178000.i2c/i2c-4/4-0038/fts_gesture_mode"
 
 # EXFAT
 TARGET_EXFAT_DRIVER := exfat
