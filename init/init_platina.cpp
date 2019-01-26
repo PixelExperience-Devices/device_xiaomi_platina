19a69ede60655910deda4f088894ce8e7ad835e0
Jago Gardiner <jagogardiner@gmail.com>
2019-01-26 17:33:50 +0000

    platina: Update init_platina.cpp

--- init/init_platina.cpp
+++ init/init_platina.cpp
@@ -1,6 +1,6 @@
 /*
-   Copyright (c) 2015, The Linux Foundation. All rights reserved.
-   Copyright (C) 2016 The CyanogenMod Project.
+   Copyright (c) 2016, The CyanogenMod Project
+   Copyright (c) 2017, The LineageOS Project
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -25,33 +25,53 @@
    OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include <cstdlib>
 #include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/sysinfo.h>
 #include <unistd.h>
 
+#include <android-base/file.h>
 #include <android-base/properties.h>
-#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/_system_properties.h>
+#include <android-base/strings.h>
 
-#include "property_service.h"
 #include "vendor_init.h"
+#include "property_service.h"
 
 using android::base::GetProperty;
 using android::init::property_set;
+using android::base::ReadFileToString;
+using android::base::Trim;
 
-void property_override(char const prop[], char const value[])
+char const *heapstartsize;
+char const *heapgrowthlimit;
+char const *heapsize;
+char const *heapminfree;
+char const *heapmaxfree;
+
+static void init_finger_print_properties()
 {
-    prop_info *pi;
-     pi = (prop_info*) __system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-}
- void property_override_dual(char const system_prop[],
-        char const vendor_prop[], char const value[])
-{
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
+    std::ifstream fin;
+    std::string buf;
+
+    std::string product = GetProperty("ro.product.name", "");
+    if (product.find("platina") == std::string::npos)
+        return;
+
+    fin.open("/proc/cmdline");
+    while (std::getline(fin, buf, ' '))
+        if (buf.find("fpsensor") != std::string::npos)
+            break;
+    fin.close();
+
+    if (buf.find("fpc") != std::string::npos) {
+        property_set("ro.boot.fingerprint", "fpc");
+    } else {
+        property_set("ro.boot.fingerprint", "goodix");
+    }
 }
 
 static void init_setup_model_properties()
@@ -59,26 +79,91 @@ static void init_setup_model_properties()
     std::ifstream fin;
     std::string buf;
 
+    std::string product = GetProperty("ro.product.name", "");
+    if (product.find("platina") == std::string::npos)
+        return;
+
     fin.open("/proc/cmdline");
     while (std::getline(fin, buf, ' '))
-        if (buf.find("androidboot.hwc") != std::string::npos)
+        if (buf.find("hwversion") != std::string::npos)
             break;
     fin.close();
 
-    if (buf.find("CN") != std::string::npos) {
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "MI 8 Lite");
+    if (buf.find("2.31.0") != std::string::npos) {
+        property_set("ro.product.model", "MI 8 Lite");
     } else {
-        property_override_dual("ro.product.model", "ro.vendor.product.model",  "?MI 8 Lite");
+        property_set("ro.product.model", "MI 8 Lite");
+    }
+}
+
+static void init_alarm_boot_properties()
+{
+    char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
+    char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
+    std::string boot_reason;
+    std::string power_off_alarm;
+    std::string reboot_reason = GetProperty("ro.boot.alarmboot", "");
+
+    if (ReadFileToString(boot_reason_file, &boot_reason)
+            && ReadFileToString(power_off_alarm_file, &power_off_alarm)) {
+        /*
+         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+         * For existing PMIC chips, the following mapping applies
+         * for the value of boot_reason:
+         *
+         * 0 -> unknown
+         * 1 -> hard reset
+         * 2 -> sudden momentary power loss (SMPL)
+         * 3 -> real time clock (RTC)
+         * 4 -> DC charger inserted
+         * 5 -> USB charger inserted
+         * 6 -> PON1 pin toggled (for secondary PMICs)
+         * 7 -> CBLPWR_N pin toggled (for external power supply)
+         * 8 -> KPDPWR_N pin toggled (power key pressed)
+         */
+         if ((Trim(boot_reason) == "3" || reboot_reason == "true")
+                 && Trim(power_off_alarm) == "1") {
+             property_set("ro.alarm_boot", "true");
+         } else {
+             property_set("ro.alarm_boot", "false");
+         }
+    }
+}
+
+void check_device()
+{
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+
+    if (sys.totalram > 3072ull * 1024 * 1024) {
+        // from - phone-xxhdpi-4096-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heapminfree = "4m";
+        heapmaxfree = "8m";
+    } else if (sys.totalram > 2048ull * 1024 * 1024) {
+        // from - phone-xxhdpi-3072-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "288m";
+        heapsize = "768m";
+        heapminfree = "512k";
+	heapmaxfree = "8m";
     }
 }
 
 void vendor_load_properties()
 {
-    std::string platform;
-
-    platform = GetProperty("ro.board.platform", "");
-    if (platform != ANDROID_TARGET)
-        return;
-
+    init_alarm_boot_properties();
+    check_device();
+    init_finger_print_properties();
     init_setup_model_properties();
+
+    property_set("dalvik.vm.heapstartsize", heapstartsize);
+    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_set("dalvik.vm.heapsize", heapsize);
+    property_set("dalvik.vm.heaptargetutilization", "0.75");
+    property_set("dalvik.vm.heapminfree", heapminfree);
+    property_set("dalvik.vm.heapmaxfree", heapmaxfree);
 }
