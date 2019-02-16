2e6378e41ec3138db020931076fe45e5ee04aa73
lihonglinglory <lihonglin10086@163.com>
2019-02-16 12:49:51 +0800

    platina: update sepolicy from jason

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -225,6 +225,7 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
