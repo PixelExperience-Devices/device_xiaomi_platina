c94cfd1fffd6e1004ebb52ae9fc9eba99f370fe4
Jago Gardiner (nysadev) <jagogardiner@gmail.com>
2019-06-03 23:23:24 +0100

    treewide: Fixup dt2w
    Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,7 +152,7 @@ VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 TARGET_ENABLE_MEDIADRM_64 := true
 
 # DT2W
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/wake_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/wake_gesture"
 
 # EXFAT
 TARGET_EXFAT_DRIVER := exfat
