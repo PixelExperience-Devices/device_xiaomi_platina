c372458122067d8440bda5118dca087b0812fcd9
Bruno Rocha <brunorochaa@users.noreply.github.com>
2019-06-06 15:21:55 -0300

    fix typo

--- device.mk
+++ device.mk
@@ -29,7 +29,6 @@ $(call inherit-product, vendor/xiaomi/platina/platina-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosip
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
