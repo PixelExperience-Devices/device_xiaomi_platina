3648124389871acd5f7f1e221312f494f1c46db7
Bruno Rocha <brunorochaa@users.noreply.github.com>
2019-06-06 08:42:18 -0300

    platina: remove NoCutoutOverlay
    unecessary for Pixel Experience

--- device.mk
+++ device.mk
@@ -72,10 +72,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Overlay for disabling the notch
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
