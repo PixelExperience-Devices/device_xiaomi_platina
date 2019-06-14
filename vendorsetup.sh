87a58dba7c7b10a862a27c981553da09cbc81811
Bruno Rocha <brunoxthe2@hotmail.com>
2019-06-14 11:55:28 +0000

    fix typo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineage_platina-userdebug
-add_lunch_combo lineage_platina-eng
-add_lunch_combo lineage_platina-user
+add_lunch_combo aosp_platina-userdebug
+add_lunch_combo aosp_platina-eng
+add_lunch_combo aosp_platina-user
