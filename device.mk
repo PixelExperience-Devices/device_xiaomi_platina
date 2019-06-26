5861f0cc46fced8d978de19a063d527ec3688551
Bruno Rocha <brunorochaa@users.noreply.github.com>
2019-06-26 19:31:43 -0300

    platina: removed prebuilt gcam

--- device.mk
+++ device.mk
@@ -158,8 +158,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    libxml2 \
-    GCMod
+    libxml2
 
 # Charger
 PRODUCT_PACKAGES += \
