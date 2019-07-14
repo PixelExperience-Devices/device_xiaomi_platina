266e9c51ffaabd5acf8d00cfa2610564217f37b3
Bruno Rocha <brunorochaa@users.noreply.github.com>
2019-07-14 14:55:54 -0300

    platina: create a button jack keylayout Xiaomi sends a custom keycode while trying to adjust volume through in-headphone volume buttons, so add a custom keylayout to handle such usecase.

--- device.mk
+++ device.mk
@@ -277,7 +277,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/sdm660-tavil-snd-card_Button_Jack.kl:system/usr/keylayout/sdm660-tavil-snd-card_Button_Jack.kl
 
 # Lights
 PRODUCT_PACKAGES += \
