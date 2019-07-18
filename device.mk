c1cc7cfc2d955cc188097f6d8baf935e2a3f60d4
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-07-17 21:05:55 -0300

    platina: import and build ThermalController
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     PlaygroundMod
 
+# ThermalController app
+PRODUCT_PACKAGES += \
+    ThermalController
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
