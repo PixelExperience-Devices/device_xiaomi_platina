7107aaac4a30ce22ed4d5ba21da5d3be8962db28
Bruno Rocha <brunoxthe2@hotmail.com>
2019-09-02 12:35:41 -0300

    Revert "platina: import and build ThermalController"
    This reverts commit c1cc7cfc2d955cc188097f6d8baf935e2a3f60d4.

--- device.mk
+++ device.mk
@@ -47,10 +47,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     PlaygroundMod
 
-# ThermalController app
-PRODUCT_PACKAGES += \
-    ThermalController
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
