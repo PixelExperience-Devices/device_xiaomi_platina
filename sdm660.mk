381e9e760ed4e328ac13ebe893dcaa783397ac40
Dyneteve <dyneteve@pixelexperience.org>
2019-12-25 16:33:01 +0200

    sdm660: Add NexusLauncherRelease to PRODUCT_DEXPREOPT_SPEED_APPS
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- sdm660.mk
+++ sdm660.mk
@@ -391,8 +391,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
-# Preopt SystemUI
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
+# AOT Preload
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI \
+    NexusLauncherRelease
 
 # Privapp-Permissions
 PRODUCT_COPY_FILES += \
