e83ea600600d9e1a001be2e4a3beaea2f752cb1a
Aryan Gupta <guptaaryan189@gmail.com>
2020-01-15 07:50:34 +0000

    sdm660: Clean XiaomiDoze and Parts
    clean sepolicy
    clean rootdir
    clean hall service

--- sdm660.mk
+++ sdm660.mk
@@ -171,19 +171,11 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.9 \
     vendor.display.config@1.9_vendor
 
-# Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey
-
-# Folio
-PRODUCT_PACKAGES += \
-    folio_daemon
 
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
@@ -537,7 +529,3 @@ PRODUCT_BOOT_JARS += \
 # Wallpapers
 PRODUCT_PACKAGES += \
     PixelLiveWallpaperPrebuilt
-
-# XiaomiParts
-#PRODUCT_PACKAGES += \
-#   XiaomiParts
