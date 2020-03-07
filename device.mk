b5e1c2db9469b431596c7c0fbcbb86cdaf8bbe80
Erfan Abdi <erfangplus@gmail.com>
2020-03-07 05:30:13 +0330

    platina: Don’t enforce rro for all overlays
    * Breaking /product/overlay
    * Also this makes many platform specific overlays with current system API on vendor partition which breaks newer android version compatibilities (broken stuffs in gsis)
    
    Change-Id: I45eb18b0754726fb4f779521a0245dfcb1259b17

--- device.mk
+++ device.mk
@@ -34,8 +34,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
     $(DEVICE_PATH)/overlay-pe
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
