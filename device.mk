b156310c1b8c9cf51cb28f89f8af50ec1ae1d2d3
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    platina: Add PlatinaWifiOverlay

--- device.mk
+++ device.mk
@@ -396,5 +396,9 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(DEVICE_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Wifi Overlay
+PRODUCT_PACKAGES += \
+    PlatinaWifiOverlay
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/platina/platina-vendor.mk)
