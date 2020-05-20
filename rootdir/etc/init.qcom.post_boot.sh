0426496ceb28d933dd8bdee52fa41d072f6a7c22
Dušan Uverić <dusan.uveric9@gmail.com>
2020-05-20 16:23:50 +0200

    platina: rootdir: Always use lz4 for compression
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Change-Id: I955323fdc7ac3230bcf3c7a3af1ab54ad4e841e6

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -357,9 +357,7 @@ function configure_zram_parameters() {
         let zRamSizeMB=2048
     fi
 
-    if [ "$low_ram" == "true" ]; then
-        echo lz4 > /sys/block/zram0/comp_algorithm
-    fi
+    echo lz4 > /sys/block/zram0/comp_algorithm
 
     if [ -f /sys/block/zram0/disksize ]; then
         if [ -f /sys/block/zram0/use_dedup ]; then
