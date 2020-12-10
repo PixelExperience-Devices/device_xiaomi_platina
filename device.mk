d0b97e3624e631b81882bc099eed17a72d7983cf
sbwml <984419930@qq.com>
2020-12-10 13:49:03 +0800

    platina: Bump vendor.display.config to 2.0
    * New WFD blobs need vendor.display.confi@2.0.so

--- device.mk
+++ device.mk
@@ -189,8 +189,8 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.9 \
-    vendor.display.config@1.9_vendor
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0_vendor
 
 # Display calibration
 PRODUCT_COPY_FILES += \
