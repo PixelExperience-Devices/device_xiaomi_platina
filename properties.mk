afaa55e12e1d6778086b51edbb0acf16b1ceff7a
Brock5555 <Brock5555@users.noreply.github.com>
2021-03-22 12:46:50 +0530

    platina: Set voice call volume steps to 6
    * The DSP only supports 6 steps for in-call-volume.
    Without this change volume will revert back to full volume
    when lowest volume is selected and a new call is started (while
    the slider still remains at the lowest setting)
    
    Change-Id: Ie86b213e4e1dc13f25c1541d6b5971920ba3eb5e

--- properties.mk
+++ properties.mk
@@ -80,7 +80,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=7 \
+    ro.config.vc_call_vol_steps=6 \
     vendor.audio.adm.buffering.ms=6
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
