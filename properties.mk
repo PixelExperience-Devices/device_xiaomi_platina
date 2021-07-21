657dfb7f2787f481b0fa465238c7f20c94066b3e
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-07-21 08:25:04 -0300

    Revert "sdm660-common: Disable WiFi display for now"
    This reverts commit d411e3b057cacf92b3c378e750a03d2ebb28639b.
    Signed-off-by: allansouzadk <allansouzadk@pixelexperience.org>

--- properties.mk
+++ properties.mk
@@ -213,7 +213,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # WFD
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.debug.wfd.enable=0
+    persist.debug.wfd.enable=1
 
 # Set lmkd options
 PRODUCT_PRODUCT_PROPERTIES += \
