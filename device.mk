c1c3d00fd9524d4fb7f232f310215ec479863707
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-12 20:36:05 +0200

    platina: Stop building android.hardware.broadcastradio HIDL impl
    * Nothing uses it and it isn't even in manifest
    
    Change-Id: I7e991b1c851b80a1590df20943cf8a964e03ab28

--- device.mk
+++ device.mk
@@ -230,9 +230,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.3-service.clearkey
 
-PRODUCT_PACKAGES += \
-    android.hardware.broadcastradio@1.0-impl
-
 # fwk-detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
