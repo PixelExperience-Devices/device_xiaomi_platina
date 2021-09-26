40733675cf0e2d585b7e2916032e269a4e3a63d8
SebaUbuntu <barezzisebastiano@gmail.com>
2021-09-25 23:31:35 -0400

    platina:  libhidl: Move to Android.bp
    * Make the lib vendor_available and remove it from vndk-ext

--- device.mk
+++ device.mk
@@ -254,6 +254,7 @@ PRODUCT_COPY_FILES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
