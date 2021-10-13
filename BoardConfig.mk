e502ea357226bf10cb4304c7fcd2062d4ff3d2aa
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-10-13 03:21:37 +0000

    platina: update sepolicy flags for android 12

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,8 +148,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
