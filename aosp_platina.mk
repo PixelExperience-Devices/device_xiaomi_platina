e5f277af9b60201c6ae28d5355406262c4a386ca
Jorg3Lucas <jorgelucas@pixelexperience.org>
2022-01-30 22:13:28 +0000

    platina: Remove build fingerprint from makefile

--- aosp_platina.mk
+++ aosp_platina.mk
@@ -44,6 +44,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := platina
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="platina-user 10 QKQ1.190910.002 V12.0.3.0.QDTMIXM release-keys"
-
-BUILD_FINGERPRINT := Xiaomi/platina/platina:10/QKQ1.190910.002/V12.0.3.0.QDTMIXM:user/release-keys
+    PRODUCT_NAME=platina
