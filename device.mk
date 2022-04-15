P404-Devices/android_device_xiaomi_platina

platina: Inherit several Android Go configurations
Beneficial for everyone, even on non-Go devices.

Signed-off-by: Albert I <kras@raphielgang.org>
Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>
Change-Id: I315d5ef03c90305d2b1a44da733fcc97d3fa9583

device.mk
@@ -27,6 +27,9 @@ DEVICE_PATH := device/xiaomi/platina
 PRODUCT_COPY_FILES += \
 $(DEVICE_PATH)/configs/lm/AdaptLaunchFeature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/lm/AdaptLaunchFeature.xml \
 
+# Always preopt extracted APKs to prevent extracting out of the APK for gms modules
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
@@ -117,7 +120,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 # Dex
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
 
 # Display
 PRODUCT_PACKAGES += \
@@ -444,6 +447,10 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
+
+# Use a profile based boot image for this device
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 # Vibrator
 PRODUCT_PACKAGES += \

PixelExperience-Devices/device_xiaomi_platina

Signed-off-by: orges <me@orgesified.ml>
Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc

device.mk
@@ -372,7 +372,9 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
 # Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := everything
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 USE_DEX2OAT_DEBUG := false
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true