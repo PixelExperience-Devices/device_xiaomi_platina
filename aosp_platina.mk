221a005b0f52267c1efe7558b5d6ce466faf274e
allanpereira99 <allan.pereira.dev@gmail.com>
2022-04-15 19:14:04 +0000

    platina: disable some PixelExperience stuff

--- aosp_platina.mk
+++ aosp_platina.mk
@@ -25,7 +25,9 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # PixelExperience Properties
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_USES_AOSP_RECOVERY := true
 
 # Inherit from platina device
