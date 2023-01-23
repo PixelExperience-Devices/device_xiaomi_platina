434800a645e207a7a547a270435a82931eb312dc
Aaradhay <aaradhayvijay964@gmail.com>
2023-01-23 05:12:07 +0000

    platina: Sort overlays properly
    Change-Id: I5bb8cb5cb9a3194f7d8ba1495fd1306a541398de

--- device.mk
+++ device.mk
@@ -62,8 +62,7 @@ BOARD_HAVE_QCOM_FM := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
