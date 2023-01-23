2e8a448c1e6dab2a47ee46cee792f2f56e7754c9
SebaUbuntu <seba@sebaubuntu.dev>
2023-01-23 05:57:48 +0000

    platina: Drop product_launched_with_*.mk * It seems Google stopped making those * Directly set PRODUCT_SHIPPING_API_LEVEL
    Change-Id: I5a6cd344610bd00d6e4e91fc272754c15078c822

--- device.mk
+++ device.mk
@@ -32,6 +32,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Inherit properties.mk
 $(call inherit-product, $(DEVICE_PATH)/properties.mk)
 
+# PRODUCT_SHIPPING_API_LEVEL indicates the first api level, device has been commercially launched on.
+PRODUCT_SHIPPING_API_LEVEL := 27
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
