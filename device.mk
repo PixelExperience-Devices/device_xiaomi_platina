422b1e69738b206b5fd0e91b17806f030d71f10a
AaradhayVijay <aaradhayvijay964@gmail.com>
2023-02-01 14:37:19 +0000

    platina: Build snap

--- device.mk
+++ device.mk
@@ -43,6 +43,11 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(DEVICE_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
+
 # Consumerir
 BOARD_HAVE_IR := false
 
